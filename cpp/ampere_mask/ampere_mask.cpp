db16126d1a1b377ad706d88464f1cdb562133288
Teja10 <tsaluru@ucdavis.edu>
2021-07-30 02:03:44 -0700

    2D mask kernel

--- cpp/ampere_mask/ampere_mask.cpp
+++ cpp/ampere_mask/ampere_mask.cpp
@@ -2,16 +2,17 @@
 #include <iostream>
 #include <vector>
 
-torch::Tensor ampere_mask_cuda(const torch::Tensor input);
+torch::Tensor ampere_mask_cuda(torch::Tensor input);
 
 #define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
+// #define CHECK_DIMS(x) TORCH_CHECK(x.size().as_list().size() == 2, #x " must have rank 2")
 // TODO: Add checks for number of dims and size of dims
 // For now assume tensor comes in flat and padded to be divisible by 4
 //#define CHECK_SIZE(x) TORCH_CHECK(x.size(0) % 4 == 0, #x " must have length divisible by 4")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
-torch::Tensor ampere_mask(const torch::Tensor input) {
+torch::Tensor ampere_mask(torch::Tensor input) {
     CHECK_INPUT(input);
     return ampere_mask_cuda(input);
 }
--- cpp/ampere_mask/ampere_mask_cuda.cu
+++ cpp/ampere_mask/ampere_mask_cuda.cu
@@ -14,51 +14,66 @@ namespace F = torch::nn::functional;
 namespace {
     template <typename scalar_t>
     __global__ void ampere_cuda_kernel(
-        const torch::PackedTensorAccessor<scalar_t,1,torch::RestrictPtrTraits,size_t> input,
-        torch::PackedTensorAccessor<scalar_t,1,torch::RestrictPtrTraits,size_t> mask
+        torch::PackedTensorAccessor<scalar_t,2,torch::RestrictPtrTraits,size_t> input
     ) {
-        int tid = (blockIdx.x * blockDim.x + threadIdx.x) * 4;
-        float first = -NPP_MAXABS_32F;
-        int idx_one = -1;
-        int idx_two = -1;
-        float second = -NPP_MAXABS_32F;
-        for(int i = tid; i < tid+4; i++) {
-            if(input[i] > first) {
-                second = first;
-                idx_two = idx_one;
+        int x = (blockIdx.x * blockDim.x + threadIdx.x) * 2;
+        int y = (blockIdx.y * blockDim.y + threadIdx.y) * 2;
+        
+        
+        float first = NPP_MAXABS_32F;
+        int idx_one_x = -1;
+        int idx_one_y = -1;
+        int idx_two_x = -1;
+        int idx_two_y = -1;
+        float second = NPP_MAXABS_32F;
 
-                first = input[i];
-                idx_one = i;
-            }
-            else if(input[i] > second) {
-                second = input[i];
-                idx_two = i;
+
+        for(int i = x; i < x+2; i++) {
+            for(int j = y; j < y+2; j++) {
+                if(input[i][j] < first) {
+                    second = first;
+                    idx_two_x = idx_one_x;
+                    idx_two_y = idx_one_y;
+
+                    first = input[i][j];
+                    idx_one_x = i;
+                    idx_one_y = j;
+                }
+                else if(input[i][j] < second) {
+                    second = input[i][j];
+                    idx_two_x = i;
+                    idx_two_y = j;
+                }
             }
         }
-        mask[idx_one] = 1;
-        mask[idx_two] = 1;
+
+        input[idx_two_x][idx_two_y] = 0;
+        input[idx_one_x][idx_one_y] = 0;
     }
 }//namespace
 
-// Assume input is flat
+// Assume input is flat and padded already
 torch::Tensor ampere_mask_cuda(const torch::Tensor input) {
-    // const int pad_h = input.size(2) % 4;
-    // const int pad_w = input.size(3) % 4;
+    // const int pad_h = 4 - input.size(2) % 4;
+    // const int pad_w = 4 - input.size(3) % 4;
     // auto padded_input = F::pad(input.clone(), F::PadFuncOptions({0,pad_w,0,pad_h}));
     // auto padded_shape = input.size();
     // auto padded_input = padded_input.flatten()
-    const int num_el = input.size(0);
-    const int pad_len = 4 - num_el % 4;
-    auto padded_input = F::pad(input.clone(), F::PadFuncOptions({0,pad_len}));
-    auto mask = torch::zeros_like(padded_input);
-    const int block_size = 256;
-    const dim3 blocks((num_el / 4 + block_size - 1) / block_size);
+    // const int num_el = input.size(0);
+    // const int pad_len = 4 - num_el % 4;
+    // auto padded_input = F::pad(input.clone(), F::PadFuncOptions({0,pad_len}));
+    auto mask = torch::zeros_like(input);
+
+    // Each block reads in 16 rows and 16 columns
+    const dim3 block_size(8, 8);
+    int gridDim_x = input.size(0) / 8;
+    int gridDim_y = input.size(1) / 8;
+    const dim3 blocks(gridDim_x, gridDim_y);
     AT_DISPATCH_FLOATING_TYPES(input.type(), "ampere_cuda_kernel", ([&] {
         ampere_cuda_kernel<scalar_t><<<blocks, block_size>>>(
-            padded_input.packed_accessor<scalar_t,1,torch::RestrictPtrTraits,size_t>(),
-            mask.packed_accessor<scalar_t,1,torch::RestrictPtrTraits,size_t>()
+            input.packed_accessor<scalar_t,2,torch::RestrictPtrTraits,size_t>()
         );
     }));
-    mask = mask.index({Slice(None, num_el)});
-    return mask;
+
+    return input;
 }
