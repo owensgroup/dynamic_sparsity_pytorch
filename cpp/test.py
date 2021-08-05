c6099eb60ff23515cc09b3b344986afedb9a334a
Teja10 <tsaluru@ucdavis.edu>
2021-08-05 07:54:20 -0400

    updtage ignore file

--- cpp/test.py
+++ cpp/test.py
@@ -1,7 +1,20 @@
 import torch
-from masking import ampere_mask_cuda
+import masking
+
 
 a = torch.randn(8,8).cuda()
 print(a)
-mask = ampere_mask_cuda(a)
-print(mask)
+mask_a = masking.ampere(a, False)
+print(mask_a)
+
+b = torch.randn(2, 2, 8, 8).cuda()
+print(b)
+b = b.view(-1, 8, 8)
+mask_b = masking.ampere(b, False).view(2, 2, 8, 8)
+print(mask_b)
+
+c = torch.randn(2, 2, 8, 8).cuda()
+print(c)
+c = c.view(-1, 8, 8)
+mask_c = masking.ampere(c, True).view(2,2,8,8)
+print(mask_c)
